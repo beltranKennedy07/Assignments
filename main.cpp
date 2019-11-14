66ad95e48b18187219f53ed76af7efb618d724d0
beltranKennedy07 <56664393+beltranKennedy07@users.noreply.github.com>
2019-11-14 05:47:23 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,126 +1,71 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-string password, choice;
-double total, price;
-int points=0, items, quantity, cash, preferredItems, filledItems;
-int tries = 3, trials = 0;
+int arrSize;
 
-int wearablePoints = 10;
-int phonePoints = 30;
-int tabletsPoints = 40;
-int cameraPoints = 60;
+int input_ArrSize();
+void input_Array(string[], string [], int[], int[], string[], string[]);
+void display_Array(string [], string [], int[], int[], string[], string[]);
 
-void getPassword();
-void getDisplayMenu();
-void getTotalPoints();
-int main ()
+int main()
 {
-getPassword();
-getDisplayMenu();
-getTotalPoints();
+   string fname [5], lname [5], email [5], address [5];
+   int age [5], contact[5];
+   input_Array(fname, lname, age, contact, email, address);
+   display_Array(fname, lname, age, contact, email, address);
 
-return 0;
-}
-void getPassword()
-{
-  cout << "Please Enter your password: ";
-cin >> password;
-if(password == "123")
-{
-cout<<"Welcome user! \n";
-}
-}
-void getDisplayMenu()
-{
-cout<<"Buy more stuff to gain points and earn rewards\n ";
-cout<<"[1]Wearable\t [2]Phones\t [3]Tablets\t [4]Camera \n";
-
-cout<< "Please select an item: ";
-cin>>items;
-cout<< "quantity: ";
-cin>>quantity;
-
-points = items*quantity;
-}
-void getTotalPoints ()
-{
-cout<< "Enter the Item  price: ";
-cin >> price;
-cout<< "Enter the Item  quantity: ";
-cin>>quantity;
-
-total=quantity*price;
-
-cout<< "The total price is " << total << endl;
-cout<< "Select Items\n ";
-cout<< "[1]Wearable\t [2]Phones\t [3]Tablets\t [4]Camera \n";
-cin>>preferredItems;
-
-switch(preferredItems)
-{
-case 1:
-cout<< "Enter Amount: ";
-cin>>cash;
-filledItems = cash/wearablePoints;
-
-points = cash/100;
-cout << "The equivalent amount of the Items filled is : " << " filledItems" << endl;
-cout << "Earned Points " << points;
-break;
-case 2:
-cout<< "Enter Amount: ";
-cin>>cash;
-filledItems = cash/phonePoints;
-
-points = cash/100;
-cout << "The equivalent amount of the Items filled is : " << " filledItems " << endl;
-cout << "Earned Points " << points;
-break;
-case 3:
-cout<< "Enter Amount: ";
-cin>>cash;
-filledItems = cash/tabletsPoints;
-
-points = cash/100;
-cout << "The equivalent amount of the Items filled is : " << " filledItems" << endl;
-cout << "Earned Points " << points;
-break;
-case 4:
-cout<< "Enter Amount: ";
-cin>>cash;
-filledItems = cash/cameraPoints;
-
-points = cash/100;
-cout << "The equivalent amount of the Items filled is : " << " filledItems" << endl;
-cout << "Earned Points " << points;
-break;
-
-while(trials != 3)
-{
-cout << "Try again? Y or N: ";
-choice;
-if(choice == "Y" || "y")
-{
-trials++;
-if(trials==1)
-{
-cout << "You only have " << tries - trials << " trials remaining\n";
-}
-if(trials==2)
-{
-cout << "You only have " << tries - trials << " trials remaining\n";
-}
-if(trials==3)
-{
-cout << "You have exceeded the number of trials, try again later \n";
+    return 0;
 }
 
-}
-else{
-}
+int input_ArrSize (){
+
+    cout << "Enter array size: ";
+    cin >> arrSize;
+
+    return arrSize;
 }
 
+void input_Array(string fname[], string lname[], int age [], int contact[], string email[], string address[]){
+
+input_ArrSize();
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter Firstname: " <<i+1<<" : ";
+        cin >> fname [i];
+    }
+
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter Lastname: " <<i+1<<" : ";
+        cin >> lname [i];
+    }
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter age: " <<i+1<<" : ";
+        cin >> age [i];
+    }
+
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter contact number: " <<i+1<<" : ";
+        cin >> contact [i];
+    }
+
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter email: " <<i+1<<" : ";
+        cin >> email[i];
+    }
+
+    for (int i= 0; i<=arrSize-1; i++) {
+        cout << "Enter address: " <<i+1<<" : ";
+        cin >> address[i];
+    }
 }
-}
+
+void display_Array(string fname[], string lname [], int age[], int contact[], string email[], string address[]){
+
+system ("CLS");
+    cout << "| Firstname | Lastname | Age | Contacts | Email | Address"<< endl;
+    for (int i= 0; i<=arrSize-1; i++) {
+    cout << "\n  " << fname [i]<< "       |    " << lname[i] << "       |    " << age[i] << "       |    " << contact[i] << "       |    " << email[i]<< "       |    " << address[i] <<endl;
+
+    }
+ }
